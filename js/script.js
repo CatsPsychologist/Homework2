4699ab719f4b74852801e59fe89c278d73c0f00c
CatsPsychologist <mainofficee@gmail.com>
2021-06-10 14:36:46 +0300

    innit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let name;
+let admin;
+name='john';
+
+console.log(name);
+admin=name;
+
+alert(admin);
+//
+
+let planetEarth;
+let currentUserName;
+
+//
+let num = '10' + 10;
+
+
+console.log(num, typeof num)
+
